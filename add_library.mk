c9ee2743dce676d1d990438ef53c10f8cc5f333c
bgenia <bgenia@student.21-school.ru>
2022-03-10 13:23:52 +0300

    Remove linker_flags argument from add_library

--- add_library.mk
+++ add_library.mk
@@ -6,7 +6,7 @@
 #    By: bgenia <bgenia@student.21-school.ru>       +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/09/01 03:06:54 by bgenia            #+#    #+#              #
-#    Updated: 2021/09/28 04:57:30 by bgenia           ###   ########.fr        #
+#    Updated: 2022/03/10 13:23:28 by bgenia           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,20 +16,20 @@ _MKT_ADD_LIBRARY_MK_ := 1
 # (library_file) -> library_flag
 _MKT_get_library_flag = $(patsubst lib%.a,-l%,$(patsubst lib%.so,-l%,$1))
 
-# (path, headers, linker_flags, make_flags)
+# (path, headers, make_flags)
 define _MKT_add_library
 
 LIBS += $1
 
 LIBLDFLAGS += -L$(dir $1)
-LIBLDLIBS += $(call _MKT_get_library_flag,$(notdir $1)) $3
-LIBFLAGS += -L$(dir $1) $(call _MKT_get_library_flag,$(notdir $1)) $3
+LIBLDLIBS += $(call _MKT_get_library_flag,$(notdir $1))
+LIBFLAGS += -L$(dir $1) $(call _MKT_get_library_flag,$(notdir $1))
 LIBINCLUDES += $2
 
 .PHONY: $1 $1_clean $1_fclean $1_re
 
 $1:
-	$(MAKE) $4 -C $(dir $1)
+	$(MAKE) $3 -C $(dir $1)
 
 $1_clean:
 	$(MAKE) clean -C $(dir $1)
@@ -38,14 +38,14 @@ $1_fclean: $1_clean
 	$(MAKE) fclean -C $(dir $1)
 
 $1_re: $1_fclean
-	$(MAKE) $4 -C $(dir $1)
+	$(MAKE) $3 -C $(dir $1)
 
 endef
 
-# (path, headers, linker_flags, make_flags)
+# (path, headers, make_flags)
 define add_library
 
-$(eval $(call _MKT_add_library,$1,$2,$3,$4))
+$(eval $(call _MKT_add_library,$1,$2,$3))
 
 endef
 
