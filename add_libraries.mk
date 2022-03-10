bdb1ac3dd7e0c9fd6b8b3290bc2dd18c6f352771
bgenia <bgenia@student.21-school.ru>
2022-03-10 14:59:18 +0300

    Fix add_libraries

--- add_libraries.mk
+++ add_libraries.mk
@@ -6,7 +6,7 @@
 #    By: bgenia <bgenia@student.21-school.ru>       +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/03/10 13:24:37 by bgenia            #+#    #+#              #
-#    Updated: 2022/03/10 13:34:03 by bgenia           ###   ########.fr        #
+#    Updated: 2022/03/10 14:56:58 by bgenia           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -18,7 +18,7 @@ $(error maketools/add_libraries requires maketools/add_library to work)
 endif
 
 # (library_item)
-_MKT_add_library_from_list = $(call add_libary,$(word 1 $(subst :, ,$1)),$(word 2 $(subst :, ,$1)),$(word 3 $(subst :, ,$1)))
+_MKT_add_library_from_list = $(call add_library,$(word 1,$(subst :, ,$1)),$(word 2,$(subst :, ,$1)),$(word 3,$(subst :, ,$1)))
 
 $(foreach lib,$(.LIBRARIES),$(call _MKT_add_library_from_list,$(lib)))
 
